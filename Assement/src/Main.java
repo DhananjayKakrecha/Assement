951b64ecf59c1801b9b0d3296ae1d98b2bd199cb
Dhananjay <Dhananjay@LAPTOP-JKO2NAK2>
2023-04-27 18:52:26 +0530

    Overriding Commit

--- Assement/src/Main.java
+++ Assement/src/Main.java
@@ -8,6 +8,17 @@ class Operation {
 		return a + b + c;
 	}
 
+	int sub(int a,int b) {
+		return a+b;
+	}
+}
+
+class Subt extends Operation{
+	@Override
+	int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
 }
 
 public class Main {
@@ -15,5 +26,8 @@ public class Main {
 		Operation operation = new Operation();
 		System.out.println(operation.add(10, 2));
 		System.out.println(operation.add(12, 120, 23));
+	
+		Operation operation2 = new Subt();
+		System.out.println(operation2.sub(20, 15));
 	}
 }
